e3b11c1a5a35ac03ea87f3b53f2fa1d7f7578190
Ivan Vučica <ivan@vucica.net>
2016-05-08 00:04:32 +0100

    Change from 'Swagger: ' to 'OpenAPI: ' prefix.

--- examples/examplepb/echo_service.pb.go
+++ examples/examplepb/echo_service.pb.go
@@ -10,7 +10,7 @@ Echo Service
 Echo Service API consists of a single service which returns
 a message.
 
-Swagger: {
+OpenAPI: {
   "info": {
     "title": "Echo Service",
     "version": "1.0",
@@ -67,7 +67,7 @@ const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
 // SimpleMessage represents a simple message sent to the Echo service.
 //
-// Swagger: {
+// OpenAPI: {
 //   "externalDocs": {
 //     "url": "http://github.com/gengo/grpc-gateway",
 //     "description": "Find out more about EchoService"
@@ -103,7 +103,7 @@ type EchoServiceClient interface {
 	// The message posted as the id parameter will also be
 	// returned.
 	//
-	// Swagger: {
+	// OpenAPI: {
 	//   "externalDocs": {
 	//     "url": "http://github.com/gengo/grpc-gateway",
 	//     "description": "Find out more about EchoService"
@@ -148,7 +148,7 @@ type EchoServiceServer interface {
 	// The message posted as the id parameter will also be
 	// returned.
 	//
-	// Swagger: {
+	// OpenAPI: {
 	//   "externalDocs": {
 	//     "url": "http://github.com/gengo/grpc-gateway",
 	//     "description": "Find out more about EchoService"
